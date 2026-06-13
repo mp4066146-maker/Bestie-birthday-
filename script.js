dce19fa24ec29a71efb0c9596c079e35cf10d525
mp4066146-maker <mp4066146@gmail.com>
2026-06-13 15:45:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,24 @@ window.onload=function(){
 setTimeout(()=>{
 alert("🎉 Welcome Bestie!\n\nA Special Birthday Surprise Awaits You ❤️");
 },1000);
+  }
+for(let i=0;i<20;i++){
+
+let heart=document.createElement("div");
+heart.innerHTML="❤️";
+heart.className="heart";
+heart.style.left=Math.random()*100+"vw";
+document.body.appendChild(heart);
+
+}
+
+for(let i=0;i<15;i++){
+
+let balloon=document.createElement("div");
+balloon.innerHTML="🎈";
+balloon.className="balloon";
+balloon.style.left=Math.random()*100+"vw";
+document.body.appendChild(balloon);
+
+}
 }
