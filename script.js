c0f818848b5a4952c321dc9ee27a39fffd985a84
mp4066146-maker <mp4066146@gmail.com>
2026-06-13 15:11:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-function showGift() {
-  alert("🎉 Happy 17th Birthday Bestie! 🎂\n\nFrom Nitish Patel ❤️");
+function openGift(){
+document.getElementById("gift").style.display="block";
+
+setTimeout(()=>{
+alert("🎂 Happy 17th Birthday Bestie ❤️\n\nFrom Nitish Patel");
+},300);
+}
+
+setInterval(()=>{
+let heart=document.createElement("div");
+heart.innerHTML="❤️";
+heart.className="heart";
+heart.style.left=Math.random()*100+"vw";
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},5000);
+
+},500);
+
+setInterval(()=>{
+let balloon=document.createElement("div");
+let balloons=["🎈","🎈","🎈","🎉"];
+balloon.innerHTML=balloons[Math.floor(Math.random()*balloons.length)];
+balloon.className="balloon";
+balloon.style.left=Math.random()*100+"vw";
+
+document.body.appendChild(balloon);
+
+setTimeout(()=>{
+balloon.remove();
+},12000);
+
+},1200);
+
+window.onload=function(){
+setTimeout(()=>{
+alert("🎉 Welcome Bestie!\n\nA Special Birthday Surprise Awaits You ❤️");
+},1000);
 }
